e4472235ebe30234759490d45e5b5b77a984b81a
AndrewGalvez <dogisamoose.amazon@gmail.com>
2026-03-07 19:34:41 -0800

    fih

--- hypr/notif-song-change.sh
+++ hypr/notif-song-change.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-playerctl --player=spotify --follow metadata --format '{{title}} - {{artist}}' | while read line; do
-  dunstify -a "Spotify" "Now Playing" "$line" \
-    --icon=spotify-launcher \
+tmp="/tmp/spotify-cover.jpg"
+
+playerctl --player=spotify --follow metadata \
+  --format '{{title}} - {{artist}}|{{mpris:artUrl}}' | \
+while IFS="|" read line art; do
+  if [[ "$art" == http* ]]; then
+    curl -sL "$art" -o "$tmp"
+    icon="$tmp"
+  else
+    icon="${art#file://}"
+  fi
+
+  dunstify -t 5000 -a "Spotify" "Now Playing" "$line" \
+    --icon="$icon" \
     -A "focus,Focus" | \
   while read action; do
     [ "$action" = "focus" ] && hyprctl dispatch focuswindow class:spotify
