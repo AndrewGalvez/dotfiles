6f9a941ede2be75c52418c99c33bebf339126836
AndrewGalvez <dogisamoose.amazon@gmail.com>
2026-03-23 19:27:15 -0700

    stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,10 @@ vim.g.everforest_background = 'hard'
 vim.cmd.colorscheme("everforest");
 vim.o.background = dark
 vim.o.laststatus = 3
+
+vim.keymap.set("n", "<leader>r", function()
+	require("betterTerm").send("\x03", 1)
+	vim.defer_fn(function()
+		require("betterTerm").send("g++ main.cpp -o main && ./main\n", 1)
+	end, 100)
+end)
