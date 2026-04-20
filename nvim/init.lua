d4073c916d1b214d30b60db61707d48db8394d32
AndrewGalvez <dogisamoose.amazon@gmail.com>
2026-04-20 14:51:30 -0700

    file name autocompletion and copy pasting

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,13 +14,15 @@ vim.keymap.set("n", "<leader>r", function()
 	end, 100)
 end)
 
-vim.api.nvim_create_autocmd({"BufWinLeave"}, {
-  pattern = {"*.*"},
-  desc = "save view (folds), when closing file",
-  command = "mkview",
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+	pattern = { "*.*" },
+	desc = "save view (folds), when closing file",
+	command = "mkview",
 })
-vim.api.nvim_create_autocmd({"BufWinEnter"}, {
-  pattern = {"*.*"},
-  desc = "load view (folds), when opening file",
-  command = "silent! loadview"
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	pattern = { "*.*" },
+	desc = "load view (folds), when opening file",
+	command = "silent! loadview"
 })
+
+vim.keymap.set("i", "<Tab>", "<C-X><C-F>")
